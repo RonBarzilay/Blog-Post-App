d5e995d0eb8133cab742e8ba4bc796c66d583e0d
Ron Barzilay <ron.barzilay@gmail.com>
2023-04-13 15:33:24 +0300

    Blog logic is redy: - Node.js server using Express framework completed - Routing and routing using params implemented - EJS for passing data from server, and EJS for templating (post.ejs template vs. about/home/conntact not templates) - Rendering page to client with all data needed - Creating special page route for composing a new blog post

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lowerCase = require("lodash.lowercase");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,6 +17,45 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = [];
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
+});
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose", {});
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.PostTitle,
+    body: req.body.PostBody,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedPostName = req.params.postName;
+  posts.forEach(function (post, i) {
+    if (lowerCase(post.title) == lowerCase(requestedPostName)) {
+      res.render("post", { title: post.title, body: post.body });
+    }
+  });
 });
